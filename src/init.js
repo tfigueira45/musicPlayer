d58f2a6eaded2d56993976da433c01ea39151b20
Thiago Figueira <tfigueira45@gmail.com>
2024-01-11 11:46:58 -0300

    inicialize firebase

---
+++ src/init.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC8fwa1RIgfBmdMv9EEjAh1z7mURo8lp5E",
+  authDomain: "vernal-sunspot-397418.firebaseapp.com",
+  projectId: "vernal-sunspot-397418",
+  storageBucket: "vernal-sunspot-397418.appspot.com",
+  messagingSenderId: "835186792700",
+  appId: "1:835186792700:web:052e0c96f3a23c23d35f79",
+  measurementId: "G-KLEKQJVCWW"
+};
+
+const app = initializeApp(firebaseConfig);
