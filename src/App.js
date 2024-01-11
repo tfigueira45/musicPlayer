07f7277e536f3d82713d25f1be9bbc1d70fbc939
Thiago Figueira <tfigueira45@gmail.com>
2024-01-11 13:23:06 -0300

    create basic routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter, Routes ,Route } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" exact Component={Home} />
+          <Route path="/login" Component={Login} /> 
+        </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+        <>
+            <h1>Home</h1>
+            <Link to="/login">Login</Link>
+        </>
+
+    )
+}
+
+export default Home;
